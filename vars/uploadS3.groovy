972ae4eacaed9e87588617e9c030af5d6317ab28
engineermavs <87467228+engineermavs@users.noreply.github.com>
2021-10-11 22:08:09 +0800

    Update and rename uploadS3 to uploadS3.groovy

--- vars/uploadS3.groovy
+++ vars/uploadS3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "test-bucket-10112021"]) {
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "test-bucket-mrm"]) {
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
