e2adca59bcf4f3edd0e00887fdee4cfd37f8a923
engineermavs <87467228+engineermavs@users.noreply.github.com>
2021-10-12 15:24:31 +0800

    Update uploadS3.groovy

--- vars/uploadS3.groovy
+++ vars/uploadS3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "test-bucket-mrm"]) {
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-mavs"]) {
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
