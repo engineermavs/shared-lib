85afd2902c091c901beeb611b24598950a21f5d4
engineermavs <87467228+engineermavs@users.noreply.github.com>
2021-10-13 17:17:26 +0800

    Update uploadS3.groovy

--- vars/uploadS3.groovy
+++ vars/uploadS3.groovy
@@ -1,9 +1,9 @@
 def call(Map stageParams = [:]) {
-    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+    withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
-            includePathPattern: "*.yml, .txt",
-            bucket: stageParams.s3Bucket
+            includePathPattern: "${stageParams.path}",
+            bucket: "${stageParams.s3Bucket}"
         )
     }
 }
