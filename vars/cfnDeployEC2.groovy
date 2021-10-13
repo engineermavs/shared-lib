9ad43ec8477aafa2da1ca9521c3b8921c22ca039
engineermavs <87467228+engineermavs@users.noreply.github.com>
2021-10-13 14:24:10 +0800

    Update cfnDeployEC2.groovy

--- vars/cfnDeployEC2.groovy
+++ vars/cfnDeployEC2.groovy
@@ -2,12 +2,12 @@ def call(Map stageParams = [awsRegion: "us-east-1"]){
   withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         cfnCreateChangeSet(
-          stack:'EC2Jenkins-mavs', 
+          stack:'EC2Jenkins-mrm', 
           changeSet:'my-change-set', 
-          url:'https://testbucket-mavs.s3.amazonaws.com/deployEC2.yml'
+          url:'https://testbucket-mrm.s3.amazonaws.com/deployEC2.yml'
         )
         cfnExecuteChangeSet(
-          stack:'EC2Jenkins-mavs', 
+          stack:'EC2Jenkins-mrm', 
           changeSet:'my-change-set'
         )
   }
