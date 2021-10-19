ef2cb19076bf73798a803742b961a4ca764cf3ac
engineermavs <87467228+engineermavs@users.noreply.github.com>
2021-10-19 17:21:58 +0800

    Update deployStack.groovy

--- vars/deployStack.groovy
+++ vars/deployStack.groovy
@@ -1,15 +1,22 @@
-def call(Map stageParams = [:]){
-  withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {
+def call(Map stageParams = [awsRegion: "us-east-1"]){
+  withAWS(region: stageParams.awsRegion, credentials:"awscredentials") {
         awsIdentity()
         cfnCreateChangeSet(
-          stack: "${stageParams.stack}", 
+          stack: stageParams.stackName, 
           changeSet:'my-change-set', 
-          url: "${stageParams.url}"
+          tags:['Environment='+stageParams.env],
+		      params:['VpcId='+stageParams.VpcId, 'PublicSubnet1='+stageParams.PublicSubnet1, 'PublicSubnet2='+stageParams.PublicSubnet2],
+          url:'https://'+ stageParams.bucketName + '.s3.amazonaws.com/'+ stageParams.stackFileName
         )
-        cfnExecuteChangeSet(
-          stack:"${stageParams.stack}", 
-          changeSet:'my-change-set'
+		cfnExecuteChangeSet(
+          stack: stageParams.stackName, 
+          changeSet:'my-change-set',
+	  pollInterval: 1000,
+	  timeoutInMinutes: 60
         )
+		outputs = cfnDescribe(stack:stageParams.stackName)
+		outputs.each { println it }
+
   }
 }
 //awsRegion: "us-east-1" , 'EC2Jenkins-mavs', 'https://testbucket-mavs.s3.amazonaws.com/deployEC2.yml'
